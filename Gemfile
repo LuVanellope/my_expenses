c3b9e1af9edf29babdb12d9bcf0ad28ed2fc848f
Lu <luacosta.fc@gmail.com>
2018-04-05 20:44:26 -0500

    Add gem faker,factory_bot_rails, modified: models: expense, expense_spec and rails_helper

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
+  gem 'faker'
+  gem 'factory_bot_rails'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Adds support for Capybara system testing and selenium driver
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -4,8 +4,11 @@ RSpec.describe Expense, type: :model do
   context 'the expense exists' do
     it 'should belong to user' do
       time = Time.now.freeze
-      expense = Expense.new(amount: 30000, concept: 'uber', date: time )
-      expect(expense.save)to eq(true) 
+      user = User.create!(email: Faker::Internet.email, password: Faker::Internet.password(6))
+      puts user.inspect
+      expense = Expense.new(amount: 30000, concept: 'uber', date: time, user_id: user.id)
+      expect(expense.valid?).to eq(true) 
+
     end
   end
 end
