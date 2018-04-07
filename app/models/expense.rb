05d9cae4f3f937662782be4ef2ca8e8a4eab08b6
Lu <luacosta.fc@gmail.com>
2018-04-06 20:23:52 -0500

    adding new test date and concept

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
+  validates :amount, numericality: {greater_than: 0}
+  validates :concept, presence: true 
+
 end
