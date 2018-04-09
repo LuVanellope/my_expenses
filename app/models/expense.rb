46a5f3a47bb4ac85fa56c6273ad9d61658b846d5
Lu <luacosta.fc@gmail.com>
2018-04-09 18:38:46 -0500

    changes on category

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,13 @@
 class Expense < ApplicationRecord
-  belongs_to :user
+  belongs_to :category  
+  has_one :category
   validates :user_id, presence: true
   validates :amount, numericality: {greater_than: 0}
   validates :concept, presence: true 
-
+  
+  def date_cant_be_nil
+    if self.date.nil?
+      self.date = time.now
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180407021032) do
+ActiveRecord::Schema.define(version: 20180407022016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20180407021032) do
     t.datetime "date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_expenses_on_category_id"
     t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
@@ -48,5 +50,6 @@ ActiveRecord::Schema.define(version: 20180407021032) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "expenses", "categories"
   add_foreign_key "expenses", "users"
 end
